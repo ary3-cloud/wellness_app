dd78d6587c5e69af344994d4e67ef7916a8a00a8
ary3-cloud <marynjenga823@gmail.com>
2026-04-07 10:33:19 +0300

    Added more buttions and activities

--- app/src/main/java/com/mary/chui_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/mary/chui_wellnessapp/MainActivity.kt
@@ -49,5 +49,42 @@ class MainActivity : AppCompatActivity() {
             val meditationIntent= Intent(applicationContext, Meditation::class.java)
             startActivity(meditationIntent)
         }
+
+//        hydration alert
+        val hydration=findViewById<Button>(R.id.hydration)
+//        SET ONCLICK LISENER
+        hydration.setOnClickListener {
+            val hydrationIntent= Intent(applicationContext,HydrationAlert::class.java)
+            startActivity(hydrationIntent)
+        }
+
+//        start activity intent
+        val exercise=findViewById<Button>(R.id.exercise)
+        exercise.setOnClickListener {
+            val exerciseIntent= Intent(applicationContext, StartExercise::class.java)
+            startActivity(exerciseIntent)
+        }
+
+//        daily motivation intent
+        val motivation=findViewById<Button>(R.id.motivation)
+        motivation.setOnClickListener {
+            val motivationIntent= Intent(applicationContext,DailyMotivation::class.java)
+            startActivity(motivationIntent)
+        }
+
+//        weekly goals intent
+        val goals=findViewById<Button>(R.id.goals)
+        goals.setOnClickListener {
+            val goalsIntent= Intent(applicationContext,WeeklyGoals::class.java)
+            startActivity(goalsIntent)
+        }
+
+//        check progress intent
+        val check =findViewById<Button>(R.id.check)
+        check.setOnClickListener {
+            val checkIntent= Intent(applicationContext,CheckProgress::class.java)
+            startActivity(checkIntent)
+        }
+
     }
 }
